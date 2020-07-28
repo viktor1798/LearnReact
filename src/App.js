c5fa8d0752df2214e01571756b40c6350602dc71
Viktor Semashka <viktor.semashka.18@gmail.com>
2020-07-28 20:48:09 +0300

    improvement of structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Profile from './components/Profile';
-
+import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
+import Nav from './components/Nav/Nav';
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header/>
-      <Nav/>
       <Profile/>
+      <Nav/>
     </div>
   );
 }
