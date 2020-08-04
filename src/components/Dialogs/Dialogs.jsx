ba15eff7beb1db699291fd243ef85fec59dbb7f6
Viktor Semashka <viktor.semashka.18@gmail.com>
2020-08-04 20:05:19 +0300

    layout Dialog page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,35 @@ import React from 'react';
 import cd from './Dialogs.module.css'
 
 
-const Dialogs = () =>{
-    return(
-        <div>
-            Dialogs
+const Dialogs = () => {
+    return (
+<div className={cd.dialog}>
+    <div className={cd.dialog_item}>
+        <div className={cd.item}>
+            Kolya
         </div>
+        <div className={cd.item + ' ' + cd.active}>
+            Sveta
+        </div>
+        <div className={cd.item}>
+            Igor'
+        </div>
+        <div className={cd.item}>
+            Sasha
+        </div>
+    </div>
+    <div className={cd.messages}>
+        <div className={cd.massege_item}>
+            Hi!
+        </div>
+        <div className={cd.massege_item}>
+            How are you?
+        </div>
+        <div className={cd.massege_item}>
+            Good nigth
+        </div>
+    </div>
+</div>
     )
 }
 
