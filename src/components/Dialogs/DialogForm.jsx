538044943664e72c30c4e7e3bf884802f5611ffc
Viktor Semashka <viktor.semashka.18@gmail.com>
2021-01-12 15:20:18 +0300

    an error was made if the text field was not filled

--- src/components/Dialogs/DialogForm.jsx
+++ src/components/Dialogs/DialogForm.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Field, reduxForm } from "redux-form";
+import { Textarea } from '../common/FormControl/FormControl';
+import { maxLengthCreator, required } from '../utils/validator';
 
-
+let maxLenght100 = maxLengthCreator(100)
 const DialogForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
 
             <div>
-                <Field placeholder={'Enter new message'} name={'newMessage'} component={'textarea'} />
+                <Field placeholder={'Enter new message'} name={'newMessage'} component={Textarea} validate={[required,maxLenght100]} />
             </div>
             <div>
                 {/* кнопка отправки */}
--- src/components/Profile/MyPost/PostForm.jsx
+++ src/components/Profile/MyPost/PostForm.jsx
@@ -1,11 +1,15 @@
 import { Field, reduxForm } from "redux-form";
 import React from 'react';
+import { Textarea } from "../../common/FormControl/FormControl";
+import { maxLengthCreator, required } from "../../utils/validator";
+
+let maxLenght50 = maxLengthCreator(50);
 
 const AddPostForm = (props) => {
     return (
       <form onSubmit={props.handleSubmit}>
         <div>
-          <Field placeholder={'Enter new post'} name={'addNewPost'} component={'textarea'} />
+          <Field placeholder={'Enter new post'} name={'addNewPost'} component={Textarea} validate={[required,maxLenght50 ]} />
         </div>
         <div>
           <button >Add post</button>
